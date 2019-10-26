e8d5df4e1568908162dbbe5022f75f32ba4c4134
Kamil Kucharski <kaniak274@gmail.com>
2019-10-26 20:31:45 +0200

    Add vuex

--- ebiznes/assets/router.js
+++ ebiznes/assets/router.js
@@ -1,16 +1,12 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import PostList from './components/PostList.vue';
-import PostAdd from './components/PostAdd.vue';
-
 Vue.use(Router)
 
 const router = new Router({
     mode: 'hash',
     routes: [
-        { path: '/', name: 'posts', component: PostList },
-        { path: '/add', name: 'post-add', component: PostAdd },
+
     ],
 });
 
