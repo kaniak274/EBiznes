63fda47163ab60939c31f6ca889de74aeb6a3202
Kamil Kucharski <kamil.kucharski@makimo.pl>
2019-10-30 09:11:48 +0100

    Add logout button

--- ebiznes/assets/views/Home.vue
+++ ebiznes/assets/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        <router-link :to="{ name: 'register' }">Register</router-link>
-        <router-link :to="{ name: 'login' }">Login</router-link>
-        <router-link :to="{ name: 'service-list' }">Services</router-link>
+        <div v-if="!authorizationGranted">
+            <router-link :to="{ name: 'register' }">Register</router-link>
+            <router-link :to="{ name: 'login' }">Login</router-link>
+            <router-link :to="{ name: 'service-list' }">Services</router-link>
+        </div>
+        <div v-else>
+            <logout />
+            <router-link :to="{ name: 'service-list' }">Services</router-link>
+            <router-link :to="{ name: 'service-create' }">Create service</router-link>
+        </div>
     </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
     name: 'Home',
+    computed: {
+        ...mapGetters(['authorizationGranted']),
+    },
 }
 </script>
