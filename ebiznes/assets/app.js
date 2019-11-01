392252578cf3ef2fe9b0c58cf0147ba3451d93f4
Saren <kamilsupera@interia.pl>
2019-11-01 03:50:33 +0100

    Add vuei18n translations and style for login view

--- ebiznes/assets/app.js
+++ ebiznes/assets/app.js
@@ -8,22 +8,30 @@ import store from './store/index'
 import Logout from './components/Logout';
 import Errors from './components/Errors';
 
+/* Import of file with all translations */
+import i18n from './translations';
+
 /* Import Buefy and used components */
 import 'buefy/dist/buefy.css'
 import {
+    Button,
     Field,
-    Input
+    Input,
 } from 'buefy'
+
+import './styles/main.scss';
 
 Vue.use(Toastr);
 Vue.use(Field);
 Vue.use(Input);
+Vue.use(Button);
 
 Vue.component('errors', Errors);
 Vue.component('logout', Logout);
 
 var app = new Vue({
     el: '#app',
+    i18n,
     store,
     router,
     render: h => h(App)
