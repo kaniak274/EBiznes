197fd612e0f05502c98e103d6f58b50d84b25ffb
Kamil Kucharski <kaniak274@gmail.com>
2019-11-09 16:18:15 +0100

    Check static

--- ebiznes/settings/dist.py
+++ ebiznes/settings/dist.py
@@ -5,10 +5,21 @@ DEBUG = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ALLOWED_HOSTS = [
-    'service-rent.kaniak274.usermd.net'
+    'service-rent.kaniak274.usermd.net',
+    'localhost',
 ]
 
 WEBPACK_MANIFEST_FILE = os.path.join(BASE_DIR, '../manifest-dist.json')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/dist/')
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static', 'local')
+#]
 
+ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+
+STATIC_ROOT = os.path.join(ENV_PATH, '../public/static/')
+MEDIA_ROOT = os.path.join(ENV_PATH, '../public/media/')
+
+STATICFILES_DIRS = [
+    os.path.join(ENV_PATH, '../static', 'dist')
+]
