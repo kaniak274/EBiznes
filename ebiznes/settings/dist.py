14a9f0bae85173965bc799d88d2e3a2d4df7376f
Kamil Kucharski <kaniak274@gmail.com>
2019-11-09 15:42:57 +0100

    Allowed hosts

--- ebiznes/settings/dist.py
+++ ebiznes/settings/dist.py
@@ -5,7 +5,7 @@ DEBUG = False
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 ALLOWED_HOSTS = [
-    'kaniak274.service-rent.pl',
+    'service-rent.kaniak274.usermd.net'
 ]
 
 WEBPACK_MANIFEST_FILE = os.path.join(BASE_DIR, '../manifest-dist.json')
